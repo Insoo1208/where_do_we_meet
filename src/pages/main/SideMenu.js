cfe6fafd821b07e8c652976cbcb54e19f6f4cda9
--add <--add>
2023-02-21 15:13:01 +0900

    [ 0221J ]사이드 메뉴 수정
    사이드메뉴 수정

--- src/pages/main/SideMenu.js
+++ src/pages/main/SideMenu.js
@@ -1,9 +1,36 @@
 import styled from "styled-components";
+const MenuBg = styled.div`
+  // postion: absolute;
+  // left: 0;
+  // top: 50%;
+  // bottom: 50%;
+  width: 24%;
+  height: calc(100vh - 75px);
+  background-color: #d9d9d9;
+  z-index: 10;
+`;
 
+const MenuSlideButton = styled.div`
+  postion: absolute;
+  left: 24%;
+  top: 50%;
+  bottom: 50%;
+  width: 1.56%;
+  height: 5vh;
+  background-color: #d9d9d9;
+  z-index: 10;
+
+`;
+const UserSearchEl = styled.div`
+  width: 100%;
+  heght: 
+`;
 function SideMenu () {
   
   return (
-  
+    <MenuBg>
+        test
+    </MenuBg>
   );
 }
 
