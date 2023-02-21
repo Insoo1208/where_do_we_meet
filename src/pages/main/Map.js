b592eb50c5962b336bfb9b948b60da76935f6ff6
Insoo <jincupia@gmail.com>
2023-02-21 18:20:01 +0900

    맵 띄우기

--- src/pages/main/Map.js
+++ src/pages/main/Map.js
@@ -1,9 +1,26 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+const { kakao } = window;
+
+const MapWrapper = styled.div`
+  width: 100%;
+  height: calc(100vh - 75px);
+`;
+
 function Map () {
+
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
   
   return (
-    <div>
-      
-    </div>
+    <MapWrapper id="map" />
   );
 }
 
