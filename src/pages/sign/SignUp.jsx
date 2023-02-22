9b04a1e9b929dd006ab30ebe163b96bd0c5bc634
--add <--add>
2023-02-22 18:22:42 +0900

    [ 0222J ] 회원가입
    회원가입 UI구성중

--- src/pages/sign/SignUp.jsx
+++ src/pages/sign/SignUp.jsx
@@ -1,8 +1,116 @@
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
+
+const Wrapper = styled.div`
+  width: 590px;
+  margin: 0 auto;
+  /* display: flex;
+  flex-direction: column; */
+  align-items: center;
+  padding: 16px;
+  background-color: #ffccaa;
+  h1 {
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+    padding: 1rem 2rem 1.5rem;
+    cursor: default;
+  }
+
+  h2 {
+    font-size: 28px;
+    font-weight: 100;
+    padding-bottom: .625rem;
+
+  }
+
+  .user-name {  
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* column-gap: 18px; */
+    input:first-child {
+      width: 25%;
+    }
+    input:last-child{
+      width: 25%;
+    }
+  }
+
+  .zip-code{
+    width: 30%;
+  }
+`;
+
+const StyledInput = styled.input`
+  display: block;
+  padding-left: .7rem;
+  width: 100%;
+  height: 58px;
+  margin-bottom: 1rem;
+  `;
+
+const StyledButton = styled.button`
+  width: 100%;
+  height: 58px;
+  margin-bottom: 1rem;
+`;
+
+const SpanWrapper = styled.ul`
+  display: flex;
+  
+  li {
+    font-size: 14px;
+
+    &:last-child::before {
+      content: '';
+      display: inline-block;
+      width: 1px;
+      height: .6rem;
+      background-color: #2e2e2e;
+    }
+  }
+`;
+
+
+// // 아이디로 사용가능한지 검사
+
+// const id = 'abc123';
+
+// // 알파벳 대소문자 또는 숫자로 시작하고 끝나며 4~10자리인지 검사한다.
+// /^[A-Za-z0-9]{4,10}/.test(id);
+const StyledLink = styled(Link)`
+  padding: .5rem;
+  color: black;
+`;
 function SignUp(props) {
   return (
-    <div>
-      sign up
-    </div>
+    <section style={{ padding: '150px 0' }}>
+      <Wrapper>
+        <h1>Sign Up</h1>
+        <h2>ID</h2>
+        <label htmlFor="signUnId"/>
+        <StyledInput type='text' id="signUpId" placeholder="영문 대소문자 및 숫자 6~10자리를 입력해 주세요" />
+        <h2>PASSWORD</h2>
+        <label htmlFor="signUpPw"/>
+        <StyledInput type='password' id="signUpPw" placeholder="영문 숫자 특수기호 포함, 12자 이상을 입력해 주세요" />
+        <label htmlFor="signUpPwCheck"/>
+        <StyledInput type='password' id="signUpPwCheck" placeholder="비밀번호를 입력해 주세요" />
+        <h2>NAME</h2>
+        <div className="user-name">
+          <label htmlFor="userLastName"/>
+          <StyledInput className="last-name" type='text' id="userLastName" placeholder="성을 입력해 주세요" />
+          <label htmlFor="userLastName"/>
+          <StyledInput className="first-name" type='text' id="userLastName" placeholder="이름을 입력해 주세요" />  
+        </div>
+        <h2>ADDRESS</h2>
+        <label htmlFor="userAddress"/>
+        <StyledInput className="zip-code" type='text' id="userAddress" placeholder="우편번호" disabled={true} />
+        <StyledButton>가입하기</StyledButton>
+      </Wrapper>
+    </section>
+
   );
 }
 
