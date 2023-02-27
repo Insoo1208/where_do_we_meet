ac9cb9ea5236f189c3f59fe33255e56985c34d91
--add <--add>
2023-02-27 17:29:53 +0900

    [230227j]회원가입
    id, password 정규식

--- src/pages/sign/SignUp.jsx
+++ src/pages/sign/SignUp.jsx
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 import DaumPostcode from "react-daum-postcode";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Wrapper = styled.div`
   width: 590px;
@@ -86,12 +86,25 @@ const StyledLink = styled(Link)`
 `;
 function SignUp(props) {
   // useState
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [userPasswordCheck, setUserPasswordCheck] = useState('');
   const [openPostcode, setOpenPostcode] = useState(false);
   const [postcodeValue, setpostcodeValue] = useState({
     zonecode: '',
     address: '' 
   });
+  const [inputType, setInputType] = useState('password');
   
+  useEffect(() => {
+    console.log(userPasswordCheck, '===',userPassword);
+    if(userPasswordCheck === userPassword) {
+      console.log(`비밀번호 재확인 완료!`);
+    } else {
+
+    }
+  }, [userPasswordCheck]);
+
   // 우편번호 검색
   const handleClickZipBtn = () => {
       setOpenPostcode(openPostcode => !openPostcode);
@@ -105,24 +118,74 @@ function SignUp(props) {
     setOpenPostcode(!openPostcode);
   };
 
+  // 아이디 정규식 검사
+  const idCheck = /^[a-z0-9]{6,10}/;
+  // 비밀번호 정규식 검사
+  // const passwordCheck =  /[A-Za-z0-9!@#$%^&*]{12}/g;
+  const passwordCheck =  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/ ; //최소 8 자, 하나 이상의 문자, 하나의 숫자 및 하나의 특수 문자 정규식
+  // const passwordCheck =  /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/ ;  //입력은 대소문자특문 가능하고 반드시 조합해야되는건 영문숫자만 조합 
+
+  // const inputIdCheck = document.getElementById('signUpId').onblur(idCheck.test(userId));
+  // if(inputIdCheck) {
+  //   console.log(`올바른 아이디를 입력했습니다.`);
+  // } else {
+  //   console.log(`영문 대소문자와 숫자를 이용하여 4~10자리의 아이디를 만들어 주세요.`);
+  // }
+
   return (
     <section style={{ padding: '150px 0' }}>
       <Wrapper>
         <h1>Sign Up</h1>
         <h2>ID</h2>
         <label htmlFor="signUnId"/>
-        <StyledInput type='text' id="signUpId" placeholder="영문 대소문자 및 숫자 6~10자리를 입력해 주세요" />
+        <StyledInput type='text' id="signUpId" placeholder="영문 소문자 및 숫자 6~10자리를 입력해 주세요" value={userId} autoComplete="off" required
+        onChange={(e) => {
+          setUserId(e.target.value)
+        }}
+        onBlur={() => {
+          console.log(idCheck.test(userId));
+          if(idCheck.test(userId)) {
+          console.log(`올바른 아이디를 입력했습니다.`);
+        } else {
+          console.log(`영문 대소문자와 숫자를 이용하여 6~10자리의 아이디를 만들어 주세요.`);
+        }}}
+        />
         <h2>PASSWORD</h2>
         <label htmlFor="signUpPw"/>
-        <StyledInput type='password' id="signUpPw" placeholder="영문 숫자 특수기호 포함, 12자 이상을 입력해 주세요" />
+        <StyledInput type={inputType} id="signUpPw" placeholder="반드시 영문, 숫자, 특수문자 포함 8자 이상을 입력해 주세요."  value={userPassword} autoComplete="off"
+        onChange={(e) => {
+          setUserPassword(e.target.value)
+        }}
+        onBlur={() => {
+          console.log(passwordCheck.test(userPassword));
+          if(passwordCheck.test(userPassword)) {
+          console.log(`올바른 비밀번호를 입력했습니다.`);
+        } else {
+          console.log(`반드시 영문, 숫자, 특수문자 포함 8자 이상을 입력해 주세요.`);
+        }}}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            if(inputType !== 'text') {
+              setInputType('text');
+            } else {
+              setInputType('password')
+            }
+          }}
+        >toggle</button>
         <label htmlFor="signUpPwCheck"/>
-        <StyledInput type='password' id="signUpPwCheck" placeholder="비밀번호를 입력해 주세요" />
+        <StyledInput type='password' id="signUpPwCheck" placeholder="비밀번호를 다시 입력해 주세요" value={userPasswordCheck} autoComplete="off"
+        onChange={(e) => {
+          setUserPasswordCheck(e.target.value);
+        }}
+        />
         <h2>NAME</h2>
         <div className="user-name">
           <label htmlFor="userLastName"/>
-          <StyledInput className="last-name" type='text' id="userLastName" placeholder="성을 입력해 주세요" />
+          <StyledInput className="last-name" type='text' id="userLastName" placeholder="성을 입력해 주세요" autoComplete="off" />
           <label htmlFor="userFirstName"/>
-          <StyledInput className="first-name" type='text' id="userFirstName" placeholder="이름을 입력해 주세요" />  
+          <StyledInput className="first-name" type='text' id="userFirstName" placeholder="이름을 입력해 주세요" autoComplete="off" />  
         </div>
         <h2>ADDRESS</h2>
           <div className="zip-wrapper">
