0493dc0237104716688f5e917f36b6e73298c286
--add <--add>
2023-03-09 14:34:02 +0900

    [230309j] 모달창 뒤 스크롤 막기

--- src/components/CenterModal.jsx
+++ src/components/CenterModal.jsx
@@ -119,7 +119,6 @@ const ModalButton = styled(Button)`
   }
 `; 
 
-
 // 모달
 function CenterModal({
   visible,
@@ -137,6 +136,11 @@ function CenterModal({
   const myColor = useSelector(selectColor);
 
   useEffect(() => {
+    disableScroll();
+    return () => enableScroll();
+  }, []);
+
+  useEffect(() => {
     if (localVisible && !visible) {
       setAnimate(true);
       setTimeout(() => {
@@ -145,10 +149,53 @@ function CenterModal({
     }
     setLocalVisible(visible);
   }, [localVisible, visible]);
-
+  
   
 
   if (!localVisible && !animate) return null;
+
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+  function preventDefault(e) {
+    e.preventDefault();
+  }
+
+  function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+  }
+
+  // modern Chrome requires { passive: false } when adding event
+  var supportsPassive = false;
+  try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+      get: function () { supportsPassive = true; }
+    }));
+  } catch(e) {}
+
+  var wheelOpt = supportsPassive ? { passive: false } : false;
+  var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+  // call this to Disable
+  function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+
+  // call this to Enable
+  function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+  }
+
 
   return (
     <Background disappear={!visible}>
